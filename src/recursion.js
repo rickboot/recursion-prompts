eca97e1bf79f340afd31615e78e7d80850e1f8f2
Rick Allen <rickallen@gmail.com>
2020-08-10 15:18:55 -0700

    Feature: added countOccurrence()

--- src/recursion.js
+++ src/recursion.js
@@ -327,6 +327,11 @@ var fizzBuzz = function(n) {
 // countOccurrence([2,7,4,4,1,4], 4) // 3
 // countOccurrence([2,'banana',4,4,1,'banana'], 'banana') // 2
 var countOccurrence = function(array, value) {
+  if (array.length === 0) {
+    return 0;
+  }
+  var countIncr = (array.pop() === value) ? 1 : 0;
+  return countOccurrence(array, value) + countIncr;
 };
 
 // 21. Write a recursive version of map.
